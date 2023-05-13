2a6a7a01471bc7340c3b2c27105636395dc03c88
naufalfakhar <direct.anaufal@gmail.com>
2023-05-13 17:36:47 +0700

    feat: initial setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,32 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    container: {
+      center: true,
+      padding: '1.5rem',
+    },
+    /**
+     * * Add custom screens here
+     */
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1440px',
+    },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      /**
+       * * Add custom colors here
+       */
+      colors: {
+        primary: '#14b8a6',
+        secondary: '#06b6d4',
       },
     },
   },
